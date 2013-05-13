4cbd1a91345e565e7e6e2175b9943669a45e6aee
Yaprak Ayazoğlu <yaprak.ayazoglu@gmail.com>
2013-05-13 18:37:34 +0300

    initial commit

---
+++ lib/helpers.rb
@@ -0,0 +1,8 @@
+include Nanoc::Helpers::Rendering
+include Nanoc::Helpers::Blogging
+include Nanoc::Helpers::Tagging
+include Nanoc::Helpers::LinkTo
+
+include Nanoc::Toolbox::Helpers::Disqus
+include Nanoc::Toolbox::Helpers::TaggingExtra
+include Nanoc::Toolbox::Helpers::BloggingExtra
