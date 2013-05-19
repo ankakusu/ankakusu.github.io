73422aa266f951a846f724d4f64a450d847c6f59
Yaprak Ayazoğlu <yaprak.ayazoglu@gmail.com>
2013-05-19 18:20:24 +0300

    Angularjs updates and new posts.

---
+++ js/post.js
@@ -0,0 +1,21 @@
+// $(document).ready(function() {
+	
+    // var lat     = 41.04207384890103;
+    // var lng     = 29.0972900390625;
+    // var zoom    = 11;
+    // var myOptions = {
+    //     zoom : zoom,
+    //     center : new google.maps.LatLng(lat, lng),
+    //     mapTypeId : google.maps.MapTypeId.ROADMAP
+    // };
+
+    // var map  = new google.maps.Map(document.getElementById("map-mini"), myOptions);
+
+    // var marker = new google.maps.Marker({
+    //     map: map,
+    //     position: new google.maps.LatLng(41.0367033, 27.5112240),
+    //     visible: true
+    // });
+
+
+// });
