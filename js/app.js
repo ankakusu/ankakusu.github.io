124bc71a51fb89b4f6ee5f91596875607dd1fa3c
Yaprak Ayazoğlu <yaprak.ayazoglu@gmail.com>
2013-05-21 11:15:17 +0300

    New post is modified with new google maps interaction.

--- js/app.js
+++ js/app.js
@@ -2,16 +2,9 @@
 
 /* App Module */
 
-angular.module('ya', ['google-maps']).
-    config(['$routeProvider', function($routeProvider) {
-    	$routeProvider.
-	        when('/', {templateUrl: 'index.html',   controller: mapCtrl}).
-	        when('/:picturePath/:lat/:lng'). //
-	        otherwise({redirectTo: '/'});   
-	}]).run( function($rootScope, $location) {
-
-    // register listener to watch route changes
-    $rootScope.$on( "$routeChangeSuccess", function($event, next, current) {
-      	console.log("routeChangeSuccess");         
-    });
-})
+var ya = angular.module('ya', ['google-maps']).
+	config(['$routeProvider', function($routeProvider) {
+		$routeProvider.
+	        when('/:picturePath/:lat/:lng').
+	        when('/:lat/:lng');   
+	}]);
--- js/controllers.js
+++ js/controllers.js
@@ -4,19 +4,52 @@ function mapCtrl ($scope, $http, GoogleMaps) {
     $scope.map = GoogleMaps.getMap('#map-mini');
 }
 
-function contentCtrl($scope, $location, GoogleMaps){
-	var map = GoogleMaps.getMap('#map-mini');
-	// var lat = 41.0367033;
-	// var lng = 27.5112240;
-	// $scope.itemPath = $location.$$absUrl + '#/KanolaTarlasi1.jpg/41.0367033/27.5112240';
+function contentCtrl($scope, $location, $http, GoogleMaps){
+	$scope.map = GoogleMaps.getMap('#map-mini');
+	// load the route
+	var myPath =  $location.absUrl() + "Path.gpx";
+	getRoute($http, myPath, function(){
 
-	// $scope.$on("$routeChangeSuccess", function ($event, current, previous) {
- //    		console.log("smt happened");	
-	// 	});
+	});
+	displayRoute($http, $scope.map, myPath);
 
-	console.log("I'm in contentCtrl");
+	$scope.$on("$routeChangeSuccess", function($event, next, current) {
+        if(next.params.picturePath){
+           GoogleMaps.putPicture( '#map-mini', next.params);
+        }else if(next.params.lat){
+        	GoogleMaps.putMarker('#map-mini', next.params);
+        }
+
+    });
 }
 
-function routeCtrl($scope, $location, GoogleMaps){
-	console.log("I'm in routeCtrl");
+function getRoute($http, gpxFilePath, callback){
+	$http.get(gpxFilePath).success( function(data){
+		callback(data);
+	});
+}
+
+function displayRoute($http, map, myPath){
+	$http.get(myPath).success( function(data){
+		var bounds = new google.maps.LatLngBounds();
+		var points = [];
+		$(data).find("trkpt").each(function(index, value){
+			var lat = parseFloat(value.attributes.lat.value);
+			var lon = parseFloat(value.attributes.lon.value);
+			var p  = new google.maps.LatLng(lat, lon);
+			points.push(p);
+			bounds.extend(p);
+		});
+
+		var polyline = new google.maps.Polyline({
+			path: points,
+			strokeColor: "#FF00AA",
+			strokeOpacity: 0.7,
+			strokeWeight: 2
+		});
+
+		polyline.setMap(map);
+		map.setCenter(points[0]);
+		map.fitBounds(bounds);
+	});
 }
