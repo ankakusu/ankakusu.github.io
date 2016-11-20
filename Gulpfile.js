f7e89d3e015ea7a9f739d98dd931ba38a38708bc
ankakusu <yaprak.ayazoglu@gmail.com>
2016-11-20 20:43:19 +0100

    Remove unused packages.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,13 +6,7 @@ var cssnext = require('postcss-cssnext');
 var sassyImport = require('postcss-sassy-import');
 var gulpPug = require('gulp-pug');
 var pug = require('pug');
-var path = require('path');
 var debug = require('gulp-debug');
-var glob = require('glob');
-var fileSystem = require('fs');
-var ejs = require('ejs');
-var marked = require('marked');
-var rename = require('gulp-rename');
 
 var config = {
     cssSrcFiles: [
@@ -38,51 +32,6 @@ var config = {
     blogTemplate: 'blog/index.pug'
 };
 
-var readPostSummary = function(fileName) {
-    var fileContent = fileSystem.readFileSync(fileName, 'utf8');
-    // fileContent = fileName;
-    var multiLineSummary = fileContent
-        .replace(/^---\r?\n[\s\S]*---\r?\n([\s\S]*)\r?\n<!--MORE-->[\s\S]*/gm, '$1')
-        .trim();
-    return multiLineSummary.replace(/(?:\r\n|\r|\n)/g, ' ');
-};
-
-var readPost = function(fileName) {
-    return {
-        'summary': readPostSummary(fileName)
-    };
-};
-
-// Create a gulp task to create the blog/index.gulpPug based on the blog posts
-// available _posts folder
-var through = require('through2');
-var blogIndexGenerator = function(fileSelector) {
-    return through.obj(function(file, encoding, done) {
-        var fileNames = glob.sync(fileSelector);
-        var posts = fileNames.map(readPost);
-
-        // if (file.isBuffer()) {
-        //     console.log('is buffer');
-        // } else if (file.isStream()) {
-        //     console.log('is stream');
-        // }
-
-        // var post = readPost(file.toString('utf8'));
-
-        var blogPage = pug.renderFile(config.blogTemplate, {posts: posts});
-        // console.log(blogPage);
-        // console.log(post);
-        // console.log(file);
-        // file.contents = new Buffer(blogPage);
-        // this.push(new Buffer(blogPage));
-        // this.push(blogPage);
-        // var indexPugEjs = fileSystem.readFileSync('blog/index.gulpPug.ejs', 'utf8');
-        // var indexPug = ejs.render(indexPugEjs, {'posts': posts});
-        fileSystem.writeFileSync('_site/blog/index.html', blogPage);
-        done();
-    });
-};
-
 gulp.task('css', function () {
     var processors = [
         sassyImport(),
@@ -103,12 +52,6 @@ gulp.task('view', function buildHTML() {
             pretty: true
         }))
         .pipe(gulp.dest(config.viewDstFile));
-});
-
-console.log(config.viewDstFile + '/blog/');
-gulp.task('blog', function () {
-    return gulp.src(config.blogFolder)
-        .pipe(blogIndexGenerator(config.blogSrc));
 });
 
 gulp.task('css:watch', function () {
