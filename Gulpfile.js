32bc41cbb4a9ec57eaeabdde5e709912c0e44617
ankakusu <yaprak.ayazoglu@gmail.com>
2016-11-20 20:39:32 +0100

    Update the tasks

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,9 +4,15 @@ var exec = require('gulp-exec');
 var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var sassyImport = require('postcss-sassy-import');
-var pug = require('gulp-pug');
+var gulpPug = require('gulp-pug');
+var pug = require('pug');
 var path = require('path');
 var debug = require('gulp-debug');
+var glob = require('glob');
+var fileSystem = require('fs');
+var ejs = require('ejs');
+var marked = require('marked');
+var rename = require('gulp-rename');
 
 var config = {
     cssSrcFiles: [
@@ -19,16 +25,63 @@ var config = {
     viewSrcFiles: [
         '!node_modules/**',
         '!_**/*.pug',
+        '!blog/*.pug',
         'index.pug',
         '**/*.pug'
     ],
     viewWatchFiles: [
         '**/*.pug'
     ],
-    viewDstFile: '_site'
+    viewDstFile: '_site',
+    blogFolder: '_posts',
+    blogSrc: '_posts/**/*.md',
+    blogTemplate: 'blog/index.pug'
 };
 
-console.log(config.viewSrcFiles);
+var readPostSummary = function(fileName) {
+    var fileContent = fileSystem.readFileSync(fileName, 'utf8');
+    // fileContent = fileName;
+    var multiLineSummary = fileContent
+        .replace(/^---\r?\n[\s\S]*---\r?\n([\s\S]*)\r?\n<!--MORE-->[\s\S]*/gm, '$1')
+        .trim();
+    return multiLineSummary.replace(/(?:\r\n|\r|\n)/g, ' ');
+};
+
+var readPost = function(fileName) {
+    return {
+        'summary': readPostSummary(fileName)
+    };
+};
+
+// Create a gulp task to create the blog/index.gulpPug based on the blog posts
+// available _posts folder
+var through = require('through2');
+var blogIndexGenerator = function(fileSelector) {
+    return through.obj(function(file, encoding, done) {
+        var fileNames = glob.sync(fileSelector);
+        var posts = fileNames.map(readPost);
+
+        // if (file.isBuffer()) {
+        //     console.log('is buffer');
+        // } else if (file.isStream()) {
+        //     console.log('is stream');
+        // }
+
+        // var post = readPost(file.toString('utf8'));
+
+        var blogPage = pug.renderFile(config.blogTemplate, {posts: posts});
+        // console.log(blogPage);
+        // console.log(post);
+        // console.log(file);
+        // file.contents = new Buffer(blogPage);
+        // this.push(new Buffer(blogPage));
+        // this.push(blogPage);
+        // var indexPugEjs = fileSystem.readFileSync('blog/index.gulpPug.ejs', 'utf8');
+        // var indexPug = ejs.render(indexPugEjs, {'posts': posts});
+        fileSystem.writeFileSync('_site/blog/index.html', blogPage);
+        done();
+    });
+};
 
 gulp.task('css', function () {
     var processors = [
@@ -45,15 +98,17 @@ gulp.task('css', function () {
 
 gulp.task('view', function buildHTML() {
     return gulp.src(config.viewSrcFiles)
-        .pipe(debug({title: 'unicorn:'}))
-        .pipe(pug({
+        .pipe(debug({title: 'view:'}))
+        .pipe(gulpPug({
             pretty: true
         }))
         .pipe(gulp.dest(config.viewDstFile));
 });
 
+console.log(config.viewDstFile + '/blog/');
 gulp.task('blog', function () {
-
+    return gulp.src(config.blogFolder)
+        .pipe(blogIndexGenerator(config.blogSrc));
 });
 
 gulp.task('css:watch', function () {
