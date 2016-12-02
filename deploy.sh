63cbddb402fabb432df58e6eab4d5c6f0720e823
ankakusu <yaprak.ayazoglu@gmail.com>
2016-12-02 21:32:39 +0100

    Init deploy sequence.

---
+++ deploy.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Copied from https://github.com/vy/vy.github.io/blob/source/publish.sh
+set -e
+
+isEverythingComitted() {
+	git status | grep -q "nothing to commit, working directory clean"
+}
+
+getLastCommitId() {
+	git log -1 | head -n 1 | awk '{print $2}'
+}
+
+echo "*** Checking repository state... "
+isEverythingComitted || {
+	echo "error: there are still changes to be committed." >&2
+	exit 1
+}
+lastCommitId=$(getLastCommitId)
+
+echo "*** Compiling..."
+node ./_dev/run.js
+
+echo "*** Switching to master..."
+git checkout master
+
+echo "*** Syncing output files..."
+rsync --exclude-from rsync-excludes.txt -a --delete --force _site/ .
+
+echo "*** Adding all output files to the repository..."
+git add .
+
+echo "*** Committing changes..."
+git commit -a -m "Copied changes from source/$lastCommitId."
+
+echo "*** Pushing changes..."
+git push -f origin master source
+
+echo "*** Switching back to source..."
+git checkout source
