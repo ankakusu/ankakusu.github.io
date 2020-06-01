b90a5b5751a5e4498abb75ab84c861ae59b4ddb0
Yaprak Ayazoglu <info@frontconnec.nl>
2020-06-01 21:02:18 +0200

    Try deploy script.

---
+++ _scripts/deploy.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Copied from https://github.com/vy/vy.github.io/blob/source/publish.sh
+set -e
+set -x
+
+isEverythingComitted() {
+	outputLineCount=$(git status -s | wc -l) #| grep -q "nothing to commit, working directory clean"
+	[ $outputLineCount -eq 0 ]
+}
+
+getLastCommitId() {
+	git log -1 | head -n 1 | awk '{print $2}'
+}
+
+echo "*** Checking repository state... "
+isEverythingComitted || {
+	echo "error: there are still changes to be committed." >&2
+	exit 1
+}
+lastCommitId=$(getLastCommitId)
+
+echo "*** Compiling..."
+jekyll build
+
+echo "*** Switching to master..."
+git checkout master
+
+echo "*** Syncing output files..."
+rsync --exclude-from _scripts/rsync-excludes.txt -a --delete --force _site/ .
+
+echo "*** Adding all output files to the repository..."
+git add .
+
+echo "*** Committing changes..."
+git commit -a -m "Copied changes from source/$lastCommitId."
+
+echo "*** Pushing changes..."
+git push -f origin master source
+
+echo "*** Switching back to source..."
+git checkout source
