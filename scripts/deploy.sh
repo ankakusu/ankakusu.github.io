e4e795c9b0468895056099cd07d504c07a74c87a
Yaprak Ayazoglu <info@frontconnec.nl>
2020-06-01 20:52:45 +0200

    Try deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-COMMIT_DATE=$(date '+%Y-%m-%d %H:%M:%S');
-git checkout master
-rsync -ar _site/ .
-
-git add .
-git commit -m "$COMMIT_DATE $1"
-git checkout develop
+GIT_STATUS=$(git status);
+#echo ${GIT_STATUS}
+if [[ ${GIT_STATUS} != *"On branch develop"* ]]; then
+  echo "It's there!"
+fi
+#
+#COMMIT_DATE=$(date '+%Y-%m-%d %H:%M:%S');
+#git checkout master
+#rsync -ar _site/ .
+#
+#git add .
+#git commit -m "$COMMIT_DATE $1"
+#git checkout develop
